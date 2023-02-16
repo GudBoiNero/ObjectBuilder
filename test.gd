11e79533b1eaebb82a5a77a1d89eee7a63144bc6
GudBoiNero <donkeykong797@gmail.com>
2023-02-16 13:53:47 -0500

    Update test.gd

--- test.gd
+++ test.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+# Pass in an instance of the Object you would like to build into `new()`
+# Use `set_property()` to set any properties of your Object
+# Finish up the builder process by calling `build()` which will return your built object
 var test := ObjectBuilder.new(Sprite.new()) \
 		.set_property("texture", preload("res://icon.png")) \
 		.set_property("position", Vector2(1024/2, 600/2)) \
